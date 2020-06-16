6d0c930e1e98ac10ea9ec3b8f7e1edc80ffca0f9
gpotla <gpotla@GPOTLA-UUCBQ.cisco.com>
2020-06-16 16:17:42 -0700

    commit by gp

---
+++ guava/src/com/google/common/util/GeneralAPIKeys.java
@@ -0,0 +1,60 @@
+package com.google.common.util;
+
+public interface GeneralAPIKeys {
+
+	// Image Uploaders
+	public static final String ImageShackKey = "47BDEOSU2e5bd1f5ce9a80238aec942c1ce42b83";
+	public static final String TinyPicID = "e2aabb8d555322fa";
+	public static final String TinyPicKey = "00a68ed73ddd54da52dc2d5803fa35ee";
+	/*
+	 * public static final String ImgurAnonymousKey =
+	 * "af2fde9818ae53e7670ab52fb8ade644"; public static final String
+	 * ImgurConsumerKey = "7daf4350229dc1fd8a9d5f8a2302094004fab40cf"; public static
+	 * final String ImgurConsumerSecret = "6324137e9489cc561f76c372cc5efcd3";
+	 */
+	public static final String ImgurClientID = "d297fd441566f99";
+	public static final String ImgurClientSecret = "e7d67e86cafa32e313930100607c7ed953c5ad83";
+	public static final String FlickrKey = "fbe042faa01e6af0371b6d87be75436a";
+	public static final String FlickrSecret = "9f85687dcccc7ef9";
+	public static final String PhotobucketConsumerKey = "149828681";
+	public static final String PhotobucketConsumerSecret = "d2638b653e88315aac528087e9db54e3";
+	public static final String UploadScreenshotKey = "2807828f379860848433221358";
+	public static final String ImageBamKey = "P9MMWVORXYCVM9LL";
+	public static final String ImageBamSecret = "8NHVT2W777DEHZBV54A4CIT23XJFTL1D";
+	public static final String TwitsnapsKey = "694aaea3bc8f28479c383cbcc094fb55";
+	public static final String TwitPicKey = "816528c0152003a64e45402370805a5a";
+
+	// File Uploaders
+	public static final String DropboxConsumerKey = "0te7j9ype9lrdfn";
+	public static final String DropboxConsumerSecret = "r5d3aptd9a0cwp9";
+	public static final String MinusConsumerKey = "b57b69843f7a302a276dde89890fc6";
+	public static final String MinusConsumerSecret = "3fb097f08314d713959b1f41d543b0";
+	public static final String BoxKey = "5vd8549xmn9a7zil4iupb0ciroj7cvee";
+	public static final String SendSpaceKey = "LV6OS1R0Q3";
+	public static final String DropIOKey = "6c65e2d2bfd858f7d0aa6509784f876483582eea";
+	public static final String Ge_ttKey = "tdrrlb84kq2dquxrh4dw79gr3act0529";
+	public static final String JiraConsumerKey = "NJgrJ4EhAahDwYuxLTtT"; // Random generated string
+
+	// Text Uploaders
+	public static final String PastebinKey = "4b23be71ec78bbd4fb96735320aa09ef";
+	public static final String PastebinCaKey = "KxTofLKQThSBZ63Gpa7hYLlMdyQlMD6u";
+
+	// URL Shorteners
+	public static final String BitlyLogin = "jaex";
+	public static final String BitlyKey = "R_1734f57b772acb3c048eb2365075743b";
+	public static final String BitlyConsumerKey = "91e5a02e001ada9c1122f54d73bc442d9cc2a7ab";
+	public static final String BitlyConsumerSecret = "fe5f906b5f5e8114d5c266a709a9438c94e1cd3f";
+	public static final String BitlyClientID = "0bf9726bea922ab7d5760a913d6749b441e5176a";
+	public static final String BitlyClientSecret = "1156e02a228deccbe8c7f100711411063249497b";
+	public static final String KlamKey = "a4e5a8de710d80db774a8264f4588ffb";
+	public static final String ThreelyKey = "em5893833";
+
+	// Other Services
+	public static final String GoogleClientID = "810697162603.apps.googleusercontent.com";
+	public static final String GoogleClientSecret = "BzjPSjDufImJM6Dcg-WBZvoh";
+	public static final String GoogleAPIKey = "AIzaSyDjW0ATw7G-72iAoNsnkGbQKplasWG3eSA";
+	public static final String TwitterConsumerKey = "Dw9iuniatwgsk4nwOcKGNg";
+	public static final String TwitterConsumerSecret = "q2fGaWzuHbbRwQPast1lYZjFnpuiI6axTYWwTytj8";
+	public static final String PicnikKey = "3aacd2de4563b8817de708b29b5bdd0e";
+
+}
